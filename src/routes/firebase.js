e55ef70c4e77c16da18cafd19e06c877605a0259
Anay <anaypund123@gmail.com>
2025-06-12 17:47:39 +0530

    absolute path for key

--- src/routes/firebase.js
+++ src/routes/firebase.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+// const SERVICE_ACCOUNT_PATH = process.env.SERVICE_ACCOUNT_PATH;
 const admin = require("firebase-admin");
-const serviceAccount = require("../../etc/secrets/shilp-india-firebase-adminsdk-fbsvc-5af0cebbdf.json");
+const serviceAccount = require("/etc/secrets/shilp-india-firebase-adminsdk-fbsvc-5af0cebbdf.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
