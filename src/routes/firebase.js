41ccf39f699f5077987e2900d73199e6dd78711f
Anay <anaypund123@gmail.com>
2025-07-11 00:29:20 +0530

    service account path update

--- src/routes/firebase.js
+++ src/routes/firebase.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-// const SERVICE_ACCOUNT_PATH = process.env.SERVICE_ACCOUNT_PATH;
+const SERVICE_ACCOUNT_PATH = process.env.SERVICE_ACCOUNT_PATH;
 const admin = require("firebase-admin");
-const serviceAccount = require("/etc/secrets/shilp-india-firebase-adminsdk-fbsvc-5af0cebbdf.json");
+const serviceAccount = require(SERVICE_ACCOUNT_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
