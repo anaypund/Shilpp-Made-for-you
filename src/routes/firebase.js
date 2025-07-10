91404d0b3b3948760a075197b3237daf7ec022dc
Anay <anaypund123@gmail.com>
2025-07-11 01:58:44 +0530

    changes

--- src/routes/firebase.js
+++ src/routes/firebase.js
@@ -1,17 +1,7 @@
 require("dotenv").config();
-const fs = require("fs");
-const admin = require("firebase-admin");
-
 const SERVICE_ACCOUNT_PATH = process.env.SERVICE_ACCOUNT_PATH;
-
-let serviceAccount;
-try {
-  serviceAccount = JSON.parse(fs.readFileSync(SERVICE_ACCOUNT_PATH, "utf8"));
-} catch (err) {
-  console.error("❌ Failed to load Firebase service account:", SERVICE_ACCOUNT_PATH);
-  console.error(err);
-  process.exit(1);
-}
+const admin = require("firebase-admin");
+const serviceAccount = require(SERVICE_ACCOUNT_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
